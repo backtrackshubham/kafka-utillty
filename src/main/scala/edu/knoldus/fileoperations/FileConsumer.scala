6587c53c585d68d17bffc990951d0d9bd7547fb2
shbham <shbham.verma@knoldus.in>
2019-08-06 23:56:10 +0530

    Added Image Header in JSON

--- src/main/scala/edu/knoldus/fileoperations/FileConsumer.scala
+++ src/main/scala/edu/knoldus/fileoperations/FileConsumer.scala
@@ -1,5 +1,6 @@
 package edu.knoldus.fileoperations
 
+import java.io.{File, FileOutputStream, OutputStream}
 import java.util.{Collections, Properties}
 
 import org.apache.kafka.clients.consumer.KafkaConsumer
@@ -10,14 +11,21 @@ object FileConsumer extends App {
   props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
   props.put("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer")
   props.put("auto.offset.reset", "earliest")
+  props.put("group.id", "something")
   props.put("enable.auto.commit", "false")
   val consumer = new KafkaConsumer[String, Array[Byte]](props)
   def readFromKafka(topic: String = "test-topic"): Unit = {
     this.consumer.subscribe(Collections.singletonList(topic))
     while (true){
       val record = consumer.poll(5000)
-      println(record)
+      record.records(topic).forEach(value => {
+        value.headers().headers("imageHeaderJson").forEach((header) => {
+          println(new String(header.value()))
+        })
+        val os: OutputStream = new FileOutputStream(new File(s"/home/freaks/Desktop/${value.key()}.png"))
+        os.write(value.value())
+      })
     }
   }
-
+  readFromKafka()
 }
--- src/main/scala/edu/knoldus/fileoperations/FileProducer.scala
+++ src/main/scala/edu/knoldus/fileoperations/FileProducer.scala
@@ -2,27 +2,63 @@ package edu.knoldus.fileoperations
 
 import java.io.File
 import java.nio.file.{Files, Paths}
+import java.util
 import java.util.Properties
 
+import edu.knoldus.model.ImageHeaderData
+import edu.knoldus.utility.DataGenerator
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.common.header.Header
+import net.liftweb.json.Serialization.write
+import net.liftweb.json._
+
 
 object FileProducer extends App {
+  implicit val formats = DefaultFormats
+
   private val props = new Properties()
   props.put("bootstrap.servers", "localhost:9092")
   props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
+  props.put("max.request.size", "1048576")
   private val producer = new KafkaProducer[String, Array[Byte]](props)
 
-//  val file = new File("/home/freaks/Pictures/Screenshot from 2019-08-06 13-08-31.png")
+  println("-----------------------------------")
 
-  val x: Array[Byte] =  Files.readAllBytes(Paths.get("/home/freaks/Pictures/Screenshot from 2019-08-06 13-08-31.png"))
-  def writeToKafka(topic: String, imageId: String, json: Array[Byte]): Unit = {
-    producer.send(new ProducerRecord[String, Array[Byte]](topic, imageId, json))
+  val fileList: List[File] = List(new File("/home/freaks/Downloads/chrome_downloads/gita/beginingEnd.png"),
+    new File("/home/freaks/Downloads/chrome_downloads/gita/karma.png"),
+    new File("/home/freaks/Downloads/chrome_downloads/gita/moksha.png"),
+    new File("/home/freaks/Downloads/chrome_downloads/gita/withoutExpectations.png"),
+    new File("/home/freaks/Downloads/chrome_downloads/gita/noJudgement.png"),
+    new File("/home/freaks/Downloads/chrome_downloads/gita/worldWhatYouImagine.png"),
+    new File("/home/freaks/Downloads/chrome_downloads/gita/constantChnage.png"),
+    new File("/home/freaks/Downloads/chrome_downloads/gita/realUnreal.png"),
+    new File("/home/freaks/Downloads/chrome_downloads/gita/goodEvil.png"),
+    new File("/home/freaks/Downloads/chrome_downloads/gita/universe.png"))
+
+  val headerList = DataGenerator.getDataToPublish.imageHeaderData
+  def writeToKafka(topic: String, imageId: String, json: Array[Byte], headers: ImageHeaderData): Unit = {
+    val head = new Header(){
+      override def key(): String = "imageHeaderJson"
+      println("converting bytes")
+      override def value(): Array[Byte] = write(headers).getBytes
+    }
+    val al = new util.ArrayList[Header]()
+    al.add(head)
+    producer.send(new ProducerRecord[String, Array[Byte]](topic, 0,imageId, json, al)).get()
   }
+  headerList.zip(fileList).foreach{
+    case (imageHeaderData: ImageHeaderData, file: File) =>
+      val byteArray = Files.readAllBytes(file.toPath)
+      println("Writing data")
+      writeToKafka("test-topic", imageHeaderData.image_Id, byteArray, imageHeaderData)
+  }
+//
+//  fileList.foreach(file => {
+//    val byteArray = Files.readAllBytes(file.toPath)
+//    val uuid = java.util.UUID.randomUUID().toString
+//    writeToKafka("test-topic", uuid, byteArray)
+//  })
 
-  println(s"\nWriting in topic: test-topic \nimageId: name\ndata: \n$x")
-  writeToKafka("test-topic", "name", x)
-
-  System.in
   def closeProducer = producer.close()
 }
