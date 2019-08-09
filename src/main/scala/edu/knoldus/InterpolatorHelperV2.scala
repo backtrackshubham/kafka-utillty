808749b48d6009999914d7e824eaf4dd670f7dcd
shbham <shbham.verma@knoldus.in>
2019-08-09 14:26:02 +0530

    remove for

--- src/main/scala/edu/knoldus/InterpolatorHelperV2.scala
+++ src/main/scala/edu/knoldus/InterpolatorHelperV2.scala
@@ -3,13 +3,17 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
 
 object InterpolatorHelperV2 extends App {
-  (for{
-    _ <- FutureHelper.publishImageHeader
-    _ <- FutureHelper.publishGPSData
-  } yield true).onComplete{
-    case Success(value) => println("Completed")
-    case Failure(value) => println("Failed")
-  }
+//  (for{
+//    _ <- FutureHelper.publishImageHeader
+//    _ <- FutureHelper.publishGPSData
+//  } yield true).onComplete{
+//    case Success(value) => println("Completed")
+//    case Failure(value) => println("Failed")
+//  }
+
+  FutureHelper.publishImageHeader
+  FutureHelper.publishGPSData
+
 
   Thread.sleep(100000)
 }
