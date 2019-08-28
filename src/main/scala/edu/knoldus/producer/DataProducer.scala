e81fdc659afe6263d51625a642bfe1a5033a9460
shbham <shbham.verma@knoldus.in>
2019-08-28 19:56:43 +0530

    Added kafak url

--- src/main/scala/edu/knoldus/producer/DataProducer.scala
+++ src/main/scala/edu/knoldus/producer/DataProducer.scala
@@ -7,15 +7,15 @@ import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
 object DataProducer {
   private val props = new Properties()
-  // props.put("bootstrap.servers", "13.90.249.246:9092")
-  props.put("bootstrap.servers", "localhost:9092")
+  props.put("bootstrap.servers", "13.90.249.246:9092")
+  // props.put("bootstrap.servers", "localhost:9092")
   props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   private val producer = new KafkaProducer[String, String](props)
 
   private val byteArrayProps = new Properties()
-   byteArrayProps.put("bootstrap.servers", "localhost:9092")
-  // byteArrayProps.put("bootstrap.servers", "13.90.249.246:9092")
+   // byteArrayProps.put("bootstrap.servers", "localhost:9092")
+  byteArrayProps.put("bootstrap.servers", "13.90.249.246:9092")
   byteArrayProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
   byteArrayProps.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
   byteArrayProps.put("max.request.size", "1048576")
