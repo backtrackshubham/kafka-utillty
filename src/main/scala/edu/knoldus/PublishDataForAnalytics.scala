f3fea9ec9d39cc077d8a38d054992cbebc668df7
shbham <shbham.verma@knoldus.in>
2019-09-30 22:00:45 +0530

    Updated code for analytics service

--- src/main/scala/edu/knoldus/PublishDataForAnalytics.scala
+++ src/main/scala/edu/knoldus/PublishDataForAnalytics.scala
@@ -1,5 +1,6 @@
 package edu.knoldus
 
+import java.text.SimpleDateFormat
 import java.util.Date
 
 import edu.knoldus.model.TrackingLabelData
@@ -14,11 +15,13 @@ object PublishDataForAnalytics extends App {
 
 //  val unitId = java.util.UUID.randomUUID().toString
   val utcMills = 3600000 + 1800000
+  val dateStr = "25-09-2019"
+  val sdt = new SimpleDateFormat("dd-MM-yyyy")
   val unitId = "73156e8b-4344-44f6-9e7b-4bf16b6aa61f"
   val llLeft = List(120,180,250)
   val durations = List(0.2,0.4,0.6)
   val trackingLabelData = TrackingLabelData("",0,2,"Person", 0,0,"imageId", "imageIdLast",0.5, 1,2,4.5,4L,"N", "W", 8)
-  val date = new Date(1568917800000L)
+  val date = sdt.parse(dateStr)
   var time = date.getTime - utcMills
   def publishData: immutable.IndexedSeq[Int] = (0 to 2000) map (index => {
     val data = trackingLabelData.copy(
